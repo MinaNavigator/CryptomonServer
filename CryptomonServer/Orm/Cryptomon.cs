edc5c76e972520ff8140fd9767dc284f76f44c37
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-12 19:46:11 +0100

    add monster table

--- CryptomonServer/Orm/Cryptomon.cs
+++ CryptomonServer/Orm/Cryptomon.cs
@@ -1,10 +1,20 @@
 ﻿namespace CryptomonServer.Orm
 {
+    [Flags]
+    public enum CryptomonType
+    {
+        Nothing = 0,
+        Fire = 1,
+        Water = 2,
+        Earth = 3,
+        Wind = 4
+    }
+
     public class Cryptomon
     {
         public int CryptomonId { get; set; }
         public string Name { get; set; }
-        public int CryptomonType { get; set; }
+        public CryptomonType CryptomonType { get; set; }
         public int Hp { get; set; }
         public int Atk { get; set; }
         public int Def { get; set; }
---
+++ CryptomonServer/Orm/Monster.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptomonServer.Orm
+{
+    public class Monster
+    {
+        public int MonsterId { get; set; }
+        public int CryptomonId { get; set; }
+        public int ActualHp { get; set; }
+        public int Level { get; set; }
+
+        public virtual Cryptomon Cryptomon { get; set; }
+    }
+}
