bc1c3b60140bdc76f24faf5b119c283478f1bc1b
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-12 19:42:41 +0100

    add cryptomon class

---
+++ CryptomonServer/Orm/Cryptomon.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptomonServer.Orm
+{
+    public class Cryptomon
+    {
+        public int CryptomonId { get; set; }
+        public string Name { get; set; }
+        public int CryptomonType { get; set; }
+        public int Hp { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+    }
+}
