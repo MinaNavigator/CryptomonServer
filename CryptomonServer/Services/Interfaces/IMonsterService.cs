61a5b897053ac67689eb910ee21bb7cab7f8f785
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-16 09:56:27 +0100

    update method signature

--- CryptomonServer/Services/Interfaces/IMonsterService.cs
+++ CryptomonServer/Services/Interfaces/IMonsterService.cs
@@ -5,9 +5,9 @@ namespace CryptomonServer.Services.Interfaces
 {
     public interface IMonsterService
     {
-        void AddMonster(int cryptomonId, int playerId);
+        Task<MonsterDto> AddMonster(int cryptomonId, int playerId);
         Task<MonsterDto> UpdateMonster(int monsterId, int hp, int exp);
-        void TransferMonster(int monsterId, int toPlayerId);
+        Task TransferMonster(int monsterId, int toPlayerId);
         Task<MonsterDto> GetMonster(int monsterId);
         Task<List<MonsterDto>> GetMonsterForPlayer(int playerId);
     }
--- CryptomonServer/Services/MonsterService.cs
+++ CryptomonServer/Services/MonsterService.cs
@@ -1,6 +1,53 @@
-﻿namespace CryptomonServer.Services
+﻿using CryptomonServer.Dtos;
+using CryptomonServer.Orm;
+using CryptomonServer.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CryptomonServer.Services
 {
-    public class MonsterService
+    public class MonsterService : IMonsterService
     {
+        private IConfiguration _config;
+        private readonly IMemoryCache _cache;
+        private readonly ILogger<MonsterService> _logger;
+        private readonly CryptomonDbContext _dbContext;
+
+        public MonsterService(ILogger<MonsterService> logger, IConfiguration config, IMemoryCache cache, CryptomonDbContext dbContext)
+        {
+            _logger = logger;
+            _config = config;
+            _cache = cache;
+            _dbContext = dbContext;
+        }
+
+        public async Task<MonsterDto> AddMonster(int cryptomonId, int playerId)
+        {
+            var cryptomon = await _dbContext.Cryptomons.SingleAsync(x => x.CryptomonId == cryptomonId);
+            var monster = new Monster { CryptomonId = cryptomonId, ActualHp = cryptomon.Hp, Level = 1 };
+            await _dbContext.AddAsync(monster);
+            await _dbContext.SaveChangesAsync();
+            return new MonsterDto();
+        }
+
+        public Task<MonsterDto> GetMonster(int monsterId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MonsterDto>> GetMonsterForPlayer(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TransferMonster(int monsterId, int toPlayerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MonsterDto> UpdateMonster(int monsterId, int hp, int exp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
