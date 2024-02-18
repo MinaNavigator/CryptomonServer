af6a0b833724594c1b4cac696d1a216e8fde5979
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-18 18:25:29 +0100

    add monster controler get service

--- CryptomonServer/Controllers/MonsterController.cs
+++ CryptomonServer/Controllers/MonsterController.cs
@@ -21,21 +21,34 @@ namespace CryptomonServer.Controllers
         private IConfiguration _config;
         private readonly IMemoryCache _cache;
         private readonly ILogger<MonsterController> _logger;
-        private readonly IAccountService _accountService;
+        private readonly IMonsterService _monsterService;
+        private readonly ICryptomonService _cryptomonService;
         private const string message = "Welcome to the mina asp auth, sign this message to authenticate {0}";
         private const string cacheConnectionKey = "GetConnection_{0}";
 
-        public MonsterController(ILogger<MonsterController> logger, IConfiguration config, IMemoryCache cache, IAccountService accountService)
+        public MonsterController(ILogger<MonsterController> logger, IConfiguration config, IMemoryCache cache, IMonsterService monsterService, ICryptomonService cryptomonService)
         {
             _logger = logger;
             _config = config;
             _cache = cache;
-            _accountService = accountService;
+            _monsterService = monsterService;
+            _cryptomonService = cryptomonService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var cryptomons = await _cryptomonService.GetAll();
+            return Ok(cryptomons);
+        }
 
-       
+         [HttpGet("GetByPlayer")]
+        public async Task<IActionResult> GetByPlayer()
+        {
 
+            var cryptomons = await _cryptomonService.GetAll();
+            return Ok(cryptomons);
+        }
 
     }
 }
