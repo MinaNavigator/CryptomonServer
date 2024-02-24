1264f2672b454f61a39b11ee5ebd3944b6ae4877
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-24 18:46:35 +0100

    update dto

--- CryptomonServer/Dtos/DepositDto.cs
+++ CryptomonServer/Dtos/DepositDto.cs
@@ -2,5 +2,8 @@
 {
     public class DepositDto
     {
+        public long DepositId { get; set; }
+        public long Amount { get; set; }
+        public string Address { get; set; }
     }
 }
--- CryptomonServer/Dtos/GameActionDto.cs
+++ CryptomonServer/Dtos/GameActionDto.cs
@@ -1,6 +1,15 @@
-﻿namespace CryptomonServer.Dtos
+﻿using CryptomonServer.Orm;
+
+namespace CryptomonServer.Dtos
 {
     public class GameActionDto
     {
+        public int ActionId { get; set; }
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+        public long Amount { get; set; }
+        public long ItemId { get; set; }
+        public long PayoutId { get; set; }
+        public ActionType ActionType { get; set; }
     }
 }
